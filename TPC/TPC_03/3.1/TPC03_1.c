7fbe8ec5b25cb602c1d9a10c6e705b4c8ef6ef1d
35sebastian <vegaa.sebastian@gmail.com>
2018-10-25 01:01:10 -0300

    TPC 3.1 OK

--- TPC/TPC_03/3.1/TPC03_1.c
+++ TPC/TPC_03/3.1/TPC03_1.c
@@ -4,33 +4,22 @@
 	\details Ejercicio:	Realizar una función que reciba un string y devuelva la cantidad de caracteres que posee.
 
 	\note esto también es para evaluar el comportamiento de Doxygen
-		
+
 	\version 1.0
 */
 #include "cabeceras.h"
 #include <string.h>
 #include <stdio.h>
 
-/*!	\fn int main ()
-	\brief Función principal
-	\details retorna la cantidad de caracteres del string.
-	\return  0
-*/
-int xtr_len (const char * string)
+int xtr_len (const char * string){
 
-{	
 	int i;
 	int a;
-	
-	for(i=0; i<20; i++)
-	{	
-		a= string[i];
-		if (a == '\0')
-		break;
+
+	for(i=0; i<20; i++){
+			a=string[i];
+			if (a == '\0') break;
 	}
-	if(i)
-	return i;
-	else
+		if(i) return i;
 	return 0;
 }
-
