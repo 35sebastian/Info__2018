b54501e32477dc05464a003d26c80e075edd9ecd
35sebastian <vegaa.sebastian@gmail.com>
2018-10-31 15:51:02 -0300

    3.2 completo

--- TPC/TPC_03/3.2/TPC03_2.c
+++ TPC/TPC_03/3.2/TPC03_2.c
@@ -2,39 +2,30 @@
 	\file TPC03_2.c
 	\brief trabajo práctico N°3 punto 2 - Info I.
 	\details Ejercicio: Realizar una función que reciba un string y un carácter e informe la cantidad de veces que éste se repite en el string.
-	
+
 	\note esta es una nota
-		
+
 	\version 1.0
 */
-
-#include <stdio.h>
 
 /*!	\fn int main ()
 	\brief Función principal
 	\details retorna la cantidad de coincidencias que tuvo el caracter ingresado en el string.
 	\return  retorna un int de la cantidad de coincidencias o 0 en caso que no la haya.
 */
+#include <stdio.h>
 
+int xtr_qtty (const char * string, char a){
 
-int xtr_qtty (const char * string, char a)
-
-{
 	int i=0, coincide=0;
-	
-	while (string[i])
-	{
-		if(a ==string[i])
-		{
-			coincide++;
-			}
-		i++;
-		
-		}
-			//	printf("el caracter ingresado coincide:%d veces en el string\n", coincide);
-		if (i!=0)
-		return coincide;
-	else
-	return 0;
-}
 
+	while (string[i]){
+					if(a ==string[i]){
+								coincide++;
+							}
+					i++;
+		}
+		if (i!=0)	return coincide;
+				else
+					return 0;
+}
