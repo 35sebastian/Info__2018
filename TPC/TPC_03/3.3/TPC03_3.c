e304f314504458ca20b018e79ca3827dfd2a427e
35sebastian <vegaa.sebastian@gmail.com>
2018-10-31 17:49:48 -0300

    modifico el TPC03.3 Falta terminar

--- TPC/TPC_03/3.3/TPC03_3.c
+++ TPC/TPC_03/3.3/TPC03_3.c
@@ -2,70 +2,57 @@
 	\file TPC03_3.c
 	\brief trabajo práctico N°3 punto 3 - Info I.
 	\details Ejercicio: Realizar una función que reciba un string y convierta todos sus caracteres a mayúscula, a minúscula o que cada palabra inicie con mayúscula y continúe con minúscula.
-	\note ojo! cuando ingresa a la funcion cambia el valor de la variable opcion a 1 
-		
+	\note ojo! cuando ingresa a la funcion cambia el valor de la variable opcion a 1
+
 	\version 1.0
 */
-
-#include <stdio.h>
 
 /*!	\fn int main ()
 	\brief Función principal
 	\details retorna la cantidad de coincidencias que tuvo el caracter ingresado en el string.
 	\return  retorna un int de la cantidad de coincidencias o 0 en caso que no la haya.
 */
-
+#include <stdio.h>
 #include <ctype.h>
 
+char * xtr_chg_case (char * string, int opcion){
 
-char * xtr_chg_case (char * string, int opcion)
-{
 	int i=0;
 	char c;
-	
-	switch(opcion)
-		{
-			case 1:
-			while (string[i])
-				{
-				c=string[i];
-				if  (islower(c)) c=toupper(c);
-				putchar (c);
-				i++;
-				}
-				break;
-			case 2:
-			while (string[i])
-				{
-					c=string[i];
-					if (isupper(c)) c=tolower(c);
-					putchar (c);
-					i++;
-				}
-				break;
-		
-			case 3:
-			
-			while (string[i])
-				{	
-					if (i==0)
-					{
-						if(string[i]>=97 && string[i]<=122)
-						c=string[i]-32;
-						else
-						c=string[i];
-						putchar (c);
-						i++;
-					}
-					c=string[i];
-					if (isupper(c)) c=tolower(c);
-					putchar (c);
-					i++;
-					
-				}
 
-	}
-	
+	switch(opcion){
+			case 1:{
+						while (string[i]){
+							c=string[i];
+						if  (islower(c)) c=toupper(c);
+						putchar (c);
+							i++;
+						}
+				break;
+			}
+			case 2:{
+							while (string[i]){
+								c=string[i];
+							if (isupper(c)) c=tolower(c);
+							putchar (c);
+								i++;
+							}
+				break;
+			}
+
+			case 3:{
+							while (string[i]){
+								if (i==0){
+										if(string[i]>=97 && string[i]<=122)	c=string[i]-32;
+											else	c=string[i];
+											putchar (c);
+												i++;
+								}
+								c=string[i];
+									if (isupper(c)) c=tolower(c);
+									putchar (c);
+										i++;
+									}
+				}
 	return 0;
 }
-
