78bc3d6bc2b964061dfae6c1b481cf83a1748040
35sebastian <vegaa.sebastian@gmail.com>
2018-11-04 22:42:32 -0300

    3.5 corregido

--- TPC/TPC_03/3.5/TPC03_5.c
+++ TPC/TPC_03/3.5/TPC03_5.c
@@ -7,14 +7,15 @@ int xtr_cmp (const char * str01, const char * str02, int opc){
 
     int cant,i,j, parcial, resultado;
 
-    printf("ingrese la cantidad de datos a leer (debe ser menor a 20):");
+  /*  printf("ingrese la cantidad de datos a leer (debe ser menor a 20):");
     scanf ("%d", &cant);
     fflush(stdin);
+*/
 
     for (i = 0, j = 0; str01[i]!='\0' && str02[j]!='\0'; i++, j++) {
       parcial=str01[i]-str02[j];
       if(parcial)
-          resultado += parcial;
+          resultado=+parcial;
     }
   return resultado;
 }
