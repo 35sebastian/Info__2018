7ad2d626ee4f3c6960f11e4f76f0f55d237939fa
35sebastian <vegaa.sebastian@gmail.com>
2018-11-04 22:21:42 -0300

    TPC3.5

---
+++ TPC/TPC_03/3.5/TPC03_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#define MAX 20
+
+int xtr_cmp (const char * str01, const char * str02, int opc){
+
+    int cant,i,j, parcial, resultado;
+
+    printf("ingrese la cantidad de datos a leer (debe ser menor a 20):");
+    scanf ("%d", &cant);
+    fflush(stdin);
+
+    for (i = 0, j = 0; str01[i]!='\0' && str02[j]!='\0'; i++, j++) {
+      parcial=str01[i]-str02[j];
+      if(parcial)
+          resultado += parcial;
+    }
+  return resultado;
+}
--- TPC/TPC_03/3.5/TPC03main.c
+++ TPC/TPC_03/3.5/TPC03main.c
@@ -2,21 +2,21 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include "cabecera.h"
+#define MAX 20
 
 int main(){
-	const char str01[20]="HolaQueTal";
-	const char str02[20]="HolaqueTal";
-	int resul;
+	const char str01[MAX]="HolaQueTal";
+	const char str02[MAX]="HolaqueTal";
+	int resul, opc;
 
-	printf ("ingrese la opcion \n0.CaseSensitive \n 1.No CaseSensitive\n");
+	printf ("ingrese la opcion \n0.CaseSensitive \n1.No CaseSensitive\n");
 	scanf("%d",&opc);
+  getchar();
 
-	resul= xtr_cmp (str01, str02);
+	resul= xtr_cmp (str01, str02, opc);
 
-	if (resul ==0)
-				printf("\nlos strings son iguales\n");
-	if(resul!=0)
-				printf("\nlos strings son distintos y su relacion es: %d\n", resul);
+	if (resul ==0) printf("\nlos strings son iguales\n");
+	       else printf("\nlos strings son distintos y su relacion es: %d\n", resul);
 
 	return 0;
 }
