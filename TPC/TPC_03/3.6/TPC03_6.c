abbc0c767b06c16b1f0efec97926fa626f1b4528
35sebastian <vegaa.sebastian@gmail.com>
2018-11-06 04:59:59 -0300

    TPC 3.6 start

---
+++ TPC/TPC_03/3.6/TPC03_6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+char *strstr(const char *haystack, const char *needle){
+
+
+
+  return resultado;
+}
---
+++ TPC/TPC_03/3.6/TPC03main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "cabecera.h"
+#define MAX 20
+
+int main(){
+	const char str01[MAX]="HolaQueTal";
+	const char str02[MAX]="HolaqueTal";
+	int resul, opc;
+
+	printf ("ingrese la opcion \n0.NO CaseSensitive \n1.CaseSensitive\n");
+	scanf("%d",&opc);
+  getchar();
+
+	resul= xtr_cmp (str01, str02, opc);
+
+	if (resul ==0) printf("\nlos strings son iguales\n");
+	       else printf("\nlos strings son distintos y su relacion es: %d\n", resul);
+
+	return 0;
+}
